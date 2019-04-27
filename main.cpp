876e6fc5ec14fa0b42c40e10de548ab9b3ac382d
LimbuBidhan <47186415+LimbuBidhan@users.noreply.github.com>
2019-04-27 16:50:42 +0800

    added comments to functions and fixed typos

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ struct records{
 };
 
 void output_menu() {
-  cout << "----------------Accouting System----------------" <<endl;
+  cout << "----------------Accounting System----------------" << endl;
   cout << "Select one of the options: " << endl;
   cout << "0: Exit program " << endl;
   cout << "1: Add records of income and expenses " << endl;
@@ -106,10 +106,10 @@ void input_data(records record[], int &num_records) {
     cout << "Please enter the type of expense i.e. Income, Utilities, Food and Groceries, Personal Care, and Entertainment" << endl;
     getline(cin,(record[num_records].type_expense));
     cout << endl;
-    cout << "Please enter the accout i.e. Cash, Credit card, Bank" << endl;
+    cout << "Please enter the account i.e. Cash, Credit card, Bank" << endl;
     getline(cin,(record[num_records].account));
     cout << endl;
-    cout << "Please enter the ammount in HKD" << endl;
+    cout << "Please enter the amount in HKD" << endl;
     cin >> record[num_records].amount;
     cout << endl;
     cout << "Date:" << record[num_records].date << "   " << "Expense:" << record[num_records].type_expense << "   " << "Account:" << record[num_records].account << "   " << "Amount:" << record[num_records].amount <<endl;
@@ -140,16 +140,21 @@ void input_budget_from_txt(int &budget){
   fin.close();
 }
 
+//delete record requested by the user 
 void delete_data(records record[], int &num_records) {
   string date;
+  //outputs all the records for user to choose the record to delete from
   for (int i=1;i<num_records;i++) {
     cout << "Date: " << record[i].date << " " << "Type of expenditure: " << record[i].type_expense << " " << "Type of account: "<< record[i].account << " " << "Amount: " << record[i].amount <<endl;
   }
   cout << "Please type in the date of the record you would like to delete: " << endl;
   cin.ignore();
+  //prompts user to choose the record to delete by inputting the date of the record
   getline(cin,date);
   for (int j=1;j<num_records;j++) {
+    //search for any record of the inputted date
     if (record[j].date==date) {
+      //if such record is found, move the position of the all records forward by one starting from deleted record
       for (int k=j;k<num_records;k++) {
         record[k].date=record[k+1].date;
         record[k].type_expense=record[k+1].type_expense;
@@ -160,26 +165,34 @@ void delete_data(records record[], int &num_records) {
   }
 }
 
+//updates the record into the "records.txt" file
 void update_record(records record[], int &num_records) {
   ofstream fout;
+  //deletes the current "records.txt" file, if any
   remove("records.txt");
   fout.open("records.txt");
+  //store the updated record into "records.txt" file
   for (int i=1;i<num_records;i++) {
     fout << setw(15) << record[i].date << setw(25) << record[i].type_expense << setw(15) << record[i].account << setw(20) << record[i].amount <<endl;
   }
   fout.close();
 }
 
+//modify the record requested by user
 void edit_data(records record[], int &num_records) {
   string date;
+  //outputs all the current records for user to modify from
   for (int i=1;i<num_records;i++) {
     cout << "Date: " << record[i].date << " " << "Type of expenditure: " << record[i].type_expense << " " << "Type of account: "<< record[i].account << " " << "Amount: " << record[i].amount <<endl;
   }
   cout << "Select the date of the record you would like to modify: " << endl;
   cin.ignore();
+  //prompts the user to choose the record to modify by inputting the date of the record
   getline(cin,date);
   for (int j=1;j<num_records;j++) {
+    //searches for the inputted date
     if (record[j].date==date) {
+      //prompts the user to input the new information for the record
       cout << "Type the new date: " << endl;
       cin >> record[j].date;
       cout << "Type the new type of expenditure: " << endl;
@@ -192,25 +205,31 @@ void edit_data(records record[], int &num_records) {
   }
 }
 
+//sort record by date 
 void sort_date(records record[],int &num_records) {
   int diff_date_count=0;
   string diff_date[MAX_NUM_RECORDS];
+  //looks for all the different inputted date in the program
   for (int i=1;i<num_records;i++) {
     int found=0;
+    //check if date already exists in array diff_date[], which consists of all different inputted dates
     for (int j=0;j<=diff_date_count;j++) {
       if (diff_date_count==0) {
         diff_date[j]=record[i].date;
         diff_date_count++;
       }
+      //if date already exists, increase the value of found (found is a variable used to check if date already exists in array diff_date[])
       else if (diff_date[j]==record[i].date) {
         found++;
       }
     }
+    //if found is 0 (date is not yet in the array diff_date[]), add the date to the array
     if (found==0) {
       diff_date[diff_date_count]=record[i].date;
       diff_date_count++;
     }
   }
+  //sort dates in ascending order
   for (int n=0;n<diff_date_count;n++) {
     for (int m=1;m<diff_date_count-1;m++) {
       if (stoi(diff_date[m].substr(6,4))<=stoi(diff_date[m+1].substr(6,4))) {
@@ -237,6 +256,7 @@ void sort_date(records record[],int &num_records) {
       }
     }
   }
+  //output the sorted record by date
   for (int k=1;k<diff_date_count;k++) {
     for (int l=1;l<num_records;l++) {
       if (record[l].date==diff_date[k]) {
@@ -246,25 +266,31 @@ void sort_date(records record[],int &num_records) {
   }
 }
 
+//sort records by account type
 void sort_account(records record[],int &num_records) {
   int diff_account_count=0;
   string diff_account[MAX_NUM_RECORDS];
+  //looks for all the different inputted account type in the program
   for (int i=1;i<num_records;i++) {
     int found=0;
+    //check if account type already exists in array diff_account[], which consists of all different inputted account type
     for (int j=0;j<=diff_account_count;j++) {
       if (diff_account_count==0) {
         diff_account[j]=record[i].account;
         diff_account_count++;
       }
+      //if account type already exists, increase the value of found (found is a variable used to check if account type already exists in array diff_account[])
       else if (diff_account[j]==record[i].account) {
         found++;
       }
     }
+    //if found is 0 (account type is not yet in the array diff_account[]), add the account type to the array
     if (found==0) {
       diff_account[diff_account_count]=record[i].account;
       diff_account_count++;
     }
   }
+  //output the sorted record by account type
   for (int k=1;k<diff_account_count;k++) {
     for (int l=1;l<num_records;l++) {
       if (record[l].account==diff_account[k]) {
@@ -274,25 +300,31 @@ void sort_account(records record[],int &num_records) {
   }
 }
 
+//sort records by type of expense
 void sort_expense(records record[],int &num_records) {
   int diff_expense_count=0;
   string diff_expense[MAX_NUM_RECORDS];
+  //looks for all the different inputted type of expense in the program
   for (int i=1;i<num_records;i++) {
     int found=0;
+    //check if type of expense already exists in array diff_expense[], which consists of all different inputted type of expense
     for (int j=0;j<=diff_expense_count;j++) {
       if (diff_expense_count==0) {
         diff_expense[j]=record[i].type_expense;
         diff_expense_count++;
       }
+      //if type of expense already exists, increase the value of found (found is a variable used to check if type of expense already exists in array diff_expense[])
       else if (diff_expense[j]==record[i].type_expense) {
         found++;
       }
     }
+    //if found is 0 (type of expense is not yet in the array diff_expense[]), add the type of expense to the array
     if (found==0) {
       diff_expense[diff_expense_count]=record[i].type_expense;
       diff_expense_count++;
     }
   }
+  //output the sorted record by type of expense
   for (int k=1;k<diff_expense_count;k++) {
     for (int l=1;l<num_records;l++) {
       if (record[l].type_expense==diff_expense[k]) {
@@ -411,6 +443,7 @@ int main() {
   int budget=0;
   input_records_from_txt (record, num_words);
   input_budget_from_txt (budget);
+  //output selection in menu if not choice is not 0 (user inputs 0 if he/she wants to exit the program)
   while (choice!=0) {
     output_menu();
     cin >> choice;
@@ -420,12 +453,12 @@ int main() {
     else if (choice==1) {
       input_data(record, num_words);
     }
-
     else if (choice==2) {
       char answer;
       cout << "Would you like to delete a record? (Y/N) " << endl;
       cin >> answer;
       if (answer=='Y') {
+        //if there is no record
         if (num_words-1==0) {
           cout<< "Cannot delete record: there is no record!" << endl;
         }
@@ -439,6 +472,7 @@ int main() {
         cout << "Would you like to edit a record? (Y/N) " << endl;
         cin >> answer;
         if (answer=='Y') {
+          //if there is no record
           if (num_words-1==0) {
             cout << "Cannot modify record: there is no record!" << endl;
           }
@@ -451,6 +485,7 @@ int main() {
           continue;
         }
       }
+      //if user inputs other characters besides 'Y' or 'N'
       else {
         cout << "Invalid input!" << endl;
       }
@@ -469,6 +504,7 @@ int main() {
       else if (answer=="type of expense") {
         sort_expense(record, num_words);
       }
+      // if user inputs anything besides 'date', 'account type' or 'type of expense'
       else {
         cout << "Invalid input!" << endl;
       }
@@ -479,6 +515,7 @@ int main() {
     else if (choice==5) {
       fin_report(record, num_words, budget);
     }
+    //if user inputs number not within the choice selection
     else if (choice<1 || choice>5) {
       cout << "Invalid input: number not within the choices " << endl;
     }
